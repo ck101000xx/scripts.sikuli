93f76cf2ad034916e9e5fc6d44a05aabe2dde90c
unknown <ck101000xx@gmail.com>
2014-10-19 13:07:11 +0800

    Continuously click "dokonisusumu"

--- scripts.py
+++ scripts.py
@@ -99,8 +99,8 @@ while True:
 #    click(wait("1413605771793.png"))
     click(wait("1413605794951.png"))
     click(wait("1413605863955.png"))
-    click(wait("1413694329127.png"))
-    click(wait(Pattern("1413694559796.png").similar(0.9)))
+    while click_if("1413694329127.png"): pass
+    click(wait(Pattern("1413694559796.png").similar(0.90)))
     while not exists("1413606152418.png", 0):
         click_if("1413615405871.png")
     click("1413606152418.png")
