4ff4ef450f0fe7648cf8ce9c780cd03ad10b81d8
ck101000xx <ck101000xx@gmail.com>
2014-10-23 14:53:24 +0800

    Initial navigator

---
+++ navigator.py
@@ -0,0 +1,24 @@
+import networkx as nx
+
+class Navigator:
+    def __init__(self, current_scene = "port"):
+        self.graph = nx.DiGraph()
+        self.current_scene = current_scene
+    def add_path(self, from_scene, to_scene, action):
+        self.graph.add_node(from_scene, to_scene, action=action)
+    def navigate_to(self, target_scene):
+        if current_scene == target_scene:
+            return
+        path = shortest_path(self.graph, self.current_scene, target_scene)
+        for from_scene, to_scene in zip(path, path[1:]):
+            self.graph[from_scene][to_scene]["action"]()
+
+navigator = Navigator()
+
+def click_port_button():
+    while exists("1413605460003.png", 0):
+        click(Location(10, 10))
+
+for scene in ["hokyu", "fixing"]:
+    navigator.add_path(scene, "port", click_port_button)
+
