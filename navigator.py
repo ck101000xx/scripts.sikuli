d2a5f719e33847427ec81422971a9ab03d41bdbc
ck101000xx <ck101000xx@gmail.com>
2014-10-24 08:39:07 +0800

    Represent scenes as enum

--- navigator.py
+++ navigator.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from enum import Enum
 
 class Navigator:
     def __init__(self, current_scene = "port"):
@@ -13,18 +14,20 @@ class Navigator:
         for from_scene, to_scene in zip(path[:-1], path):
             self.graph[from_scene][to_scene]["action"]()
 
+Scene = Enum("Scene", "hokyu port fixing")
+
 navigator = Navigator()
 
 def click_port_button():
     while exists("1413605460003.png", 0):
         click(Location(10, 10))
 
-for scene in ["hokyu", "fixing"]:
-    navigator.add_path(scene, "port", click_port_button)
+for scene in [Scene.hokyu, Scene.fixing]:
+    navigator.add_path(scene, Scene.port, click_port_button)
 
 def click_and_wait_sidebar(button):
     def nav():
         click(button)
         wait("1413605460003.png")
 
-navigator.path("port", "hokyu", click_and_wait_sidebar("1413617134671.png"))
+navigator.path(Scene.port, Scene.hokyu, click_and_wait_sidebar("1413617134671.png"))
