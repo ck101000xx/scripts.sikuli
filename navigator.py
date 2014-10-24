efb19c44b95b9041fab8ec0e69fdb3d2e773dc4a
ck101000xx <ck101000xx@gmail.com>
2014-10-24 13:31:17 +0800

    Use grph for shortest path resolving

--- navigator.py
+++ navigator.py
@@ -1,22 +1,35 @@
-import networkx as nx
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(sys.argv[0])) + "/lib/grph-1.5.24.jar")
+from sikuli import *
+from grph.in_memory import InMemoryGrph
 from enum import Enum
 
 class Navigator:
     def __init__(self, current_scene = "port"):
-        self.graph = nx.DiGraph()
+        self.graph = InMemoryGrph()
+        self.actions = {}
         self.current_scene = current_scene
     def add_path(self, from_scene, to_scene, action):
-        self.graph.add_node(from_scene, to_scene, action=action)
+        pair = from_scene.value, to_scene.value
+        self.graph.addDirectedSimpleEdge(from_scene.value, to_scene.value)
+        self.actions[pair] = action
     def navigate_to(self, target_scene):
         if current_scene == target_scene:
             return
-        path = nx.shortest_path(self.graph, self.current_scene, target_scene)
-        for from_scene, to_scene in zip(path[:-1], path):
-            self.graph[from_scene][to_scene]["action"]()
+        path = self.graph.getShortestPath( self.current_scene.value, target_scene.value).toVertexArray()
+        for edge in zip(path[:-1], path):
+            self.action[edge]()
 
 Scene = Enum("Scene", "hokyu port fixing")
 
 navigator = Navigator()
+
+def scene(scene):
+    def decorator(f):
+        navigator.navigate_to(scene)
+        f()
+    return decorator
 
 def click_port_button():
     while exists("1413605460003.png", 0):
@@ -29,5 +42,6 @@ def click_and_wait_sidebar(button):
     def nav():
         click(button)
         wait("1413605460003.png")
+    return nav
 
-navigator.path(Scene.port, Scene.hokyu, click_and_wait_sidebar("1413617134671.png"))
+navigator.add_path(Scene.port, Scene.hokyu, click_and_wait_sidebar("1413617134671.png"))
